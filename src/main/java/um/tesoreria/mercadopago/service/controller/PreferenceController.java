314e8d26582025c81c6279615e55dd459d54646c
dqmdz <daniel.quinterospinto@gmail.com>
2024-11-07 20:51:02 -0300

    Signature added

--- src/main/java/um/tesoreria/mercadopago/service/controller/PreferenceController.java
+++ src/main/java/um/tesoreria/mercadopago/service/controller/PreferenceController.java
@@ -3,7 +3,6 @@ package um.tesoreria.mercadopago.service.controller;
 import com.mercadopago.resources.payment.Payment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import um.tesoreria.mercadopago.service.domain.dto.PaymentDto;
 import um.tesoreria.mercadopago.service.service.PreferenceService;
 
 @RestController
