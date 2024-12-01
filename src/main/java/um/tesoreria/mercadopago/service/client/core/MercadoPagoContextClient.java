797a6fc47d163b7163c07ef6840ad22ab2d99427
dqmdz <daniel.quinterospinto@gmail.com>
2024-12-01 13:37:32 -0300

    Upgrading versions

--- src/main/java/um/tesoreria/mercadopago/service/client/core/MercadoPagoContextClient.java
+++ src/main/java/um/tesoreria/mercadopago/service/client/core/MercadoPagoContextClient.java
@@ -1,14 +1,23 @@
 package um.tesoreria.mercadopago.service.client.core;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import um.tesoreria.mercadopago.service.domain.dto.MercadoPagoContextDto;
+
+import java.util.List;
 
 @FeignClient(name = "tesoreria-core-service/api/tesoreria/core/mercadoPagoContext")
 public interface MercadoPagoContextClient {
 
     @GetMapping("/cuota/activo/{chequeraCuotaId}")
     MercadoPagoContextDto findActivoByChequeraCuotaId(@PathVariable Long chequeraCuotaId);
+
+    @GetMapping("/{mercadoPagoContextId}")
+    MercadoPagoContextDto findByMercadoPagoContextId(@PathVariable Long mercadoPagoContextId);
+
+    @GetMapping("/all/id")
+    List<Long> findAllIds();
 
 }
