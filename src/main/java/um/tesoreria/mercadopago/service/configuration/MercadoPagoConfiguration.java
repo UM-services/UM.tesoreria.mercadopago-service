69819219a026616b52c9487b2f5d75ebecf00618
dqmdz <daniel.quinterospinto@gmail.com>
2024-12-02 05:40:39 -0300

    Agregado del chequeo de todos los enlaces activos de MP

--- src/main/java/um/tesoreria/mercadopago/service/configuration/MercadoPagoConfiguration.java
+++ src/main/java/um/tesoreria/mercadopago/service/configuration/MercadoPagoConfiguration.java
@@ -3,9 +3,11 @@ package um.tesoreria.mercadopago.service.configuration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "um.tesoreria.mercadopago.service.client")
+@EnableScheduling
 public class MercadoPagoConfiguration {
 }
--- src/main/java/um/tesoreria/mercadopago/service/util/DateToolMP.java
+++ src/main/java/um/tesoreria/mercadopago/service/util/DateToolMP.java
@@ -3,7 +3,6 @@ package um.tesoreria.mercadopago.service.util;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 
 @Slf4j
 public class DateToolMP {
